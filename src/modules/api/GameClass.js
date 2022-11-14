9fccd17d19619d72305c85cbf7c42dd5574556a0
Hein Tay Zar <heintayzarhm@gmail.com>
2022-11-14 21:25:52 +0630

    Make Add and refresh and display functions

---
+++ src/modules/api/GameClass.js
@@ -0,0 +1,61 @@
+import { API } from './api.js';
+import { encodeHTMLEntities } from './util.js';
+
+export default class Game extends API {
+  constructor() {
+    super();
+    this.addSuccess = 'Leaderboard score created correctly.';
+    this.data = [];
+  }
+
+  add = () => {
+    const form = document.getElementById('add-score-form');
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      await this.addApi({
+        user: encodeHTMLEntities(e.target.children[0].value),
+        score: encodeHTMLEntities(e.target.children[1].value),
+      })
+        .then((res) => {
+          if (res === this.addSuccess) {
+            e.target.reset();
+            this.display();
+            this.msg('Successfully Added', true);
+          }
+        }).catch((error) => {
+          this.msg('Successfully Failed', false);
+          throw new Error(error);
+        });
+    });
+  }
+
+  refresh = async () => {
+    this.data = await this.refreshApi();
+    return this.data;
+  }
+
+  display = async () => {
+    const scoreTable = document.querySelector('.score-table');
+    scoreTable.innerHTML = '';
+    await this.refresh().then((res) => {
+      res.forEach((score) => {
+        scoreTable.innerHTML += this.score(score);
+      });
+    });
+  }
+
+  score = ({ user, score }) => `<li>${encodeHTMLEntities(user)} <span class="score">${encodeHTMLEntities(score)}</span></li>`
+
+  msg = (text, bool) => {
+    const msg = document.querySelector('.msg');
+    msg.innerHTML = '';
+    msg.innerHTML = text;
+    if (bool) {
+      msg.classList.add('success');
+      msg.classList.remove('fail');
+    } else {
+      msg.classList.remove('success');
+      msg.classList.add('fail');
+    }
+  }
+}
