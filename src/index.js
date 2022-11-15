b769757e95fe6e76f950f0400d898cb3c49c1e4e
Hein Tay Zar <heintayzarhm@gmail.com>
2022-11-15 11:21:20 +0630

    Remove inline js code

--- src/index.js
+++ src/index.js
@@ -11,7 +11,3 @@ refresh.addEventListener('click', (e) => {
   game.display();
   e.preventDefault();
 });
-
-// for the changing secret Only in development
-// import { createGames } from './modules/api/api.js';
-// createGames().then((res) => console.log(res));
