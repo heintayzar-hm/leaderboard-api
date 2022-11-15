fc2eea1d2a58d21be1cb9a75f25cdf949fe5c44c
Hein Tay Zar <heintayzarhm@gmail.com>
2022-11-15 15:20:15 +0630

    Add reload button and change folder dir

--- src/modules/GameClass.js
+++ src/modules/GameClass.js
@@ -1,4 +1,4 @@
-import { API } from './api.js';
+import { API } from './api/api.js';
 import {
   disableBtn, enableBtn, encodeHTMLEntities, onlySpaces,
 } from './util.js';
@@ -16,7 +16,7 @@ export default class Game extends API {
       e.preventDefault();
       disableBtn(e.target.children[3]);
       if (onlySpaces(e.target.children[0].value) || onlySpaces(e.target.children[1].value)) {
-        this.msg('Successfully falied no full of spaces', false);
+        this.msg('Successfully failed: no full of spaces', false);
         enableBtn(e.target.children[3]);
       } else {
         await this.addApi({
@@ -48,13 +48,16 @@ export default class Game extends API {
     const scoreTable = document.querySelector('.score-table');
     const refresh = document.querySelector('.refresh');
     disableBtn(refresh);
+    this.reload(true);
     await this.refresh().then((res) => {
       scoreTable.innerHTML = '';
+      this.reload(false);
       res.forEach((score) => {
         scoreTable.innerHTML += this.score(score);
       });
       enableBtn(refresh);
     }).catch((error) => {
+      this.reload(false);
       this.msg('Loading Failed', false);
       enableBtn(refresh);
       throw new Error(error);
@@ -73,6 +76,19 @@ export default class Game extends API {
     } else {
       msg.classList.remove('success');
       msg.classList.add('fail');
+    }
+  }
+
+  reload = (bool) => {
+    let wrapper = document.querySelector('.wrapper');
+    if (wrapper === null) {
+      document.querySelector('.score-table').innerHTML += '<li class="wrapper"><div class="loader"></div></li>';
+      wrapper = document.querySelector('.wrapper');
+    }
+    if (bool) {
+      wrapper.style.display = 'flex';
+    } else {
+      wrapper.style.display = 'none';
     }
   }
 }
